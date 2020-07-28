4fe928ace7df3cb81833a6258da86ce8967a57dd
dementorx <dementorx@outlook.com>
2020-07-28 20:42:00 +0900

    テンプレートまでやったよ

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,26 @@ def hello():
 @app.route('/top')
 def top():
     return"ここはトップだよ"
+
+@app.route('/hello/<text>')
+def  namehello(text):
+    return text +"さんこんにちわ"
+
+@app.route('/index')
+def index():
+    name="sunabaco"
+    age = 20
+    address="田町"
+
+    return render_template('index.html',user_name = name, user_address = address , user_age = age)
+
+@app.route('/weather')
+def  weather():
+    py_weather = "曇り"
+    address="雨"
+
+    return render_template('weather.html',html_weather = py_weather)
+
 
 
 
